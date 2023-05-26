a0135af6c17787caeb108be2124e7b51d7664e8d
DangMinhTien <dtien5333@gmail.com>
2023-05-26 13:28:06 +0700

    addlan3

---
+++ QLKHO/Areas/ThongKes/Controllers/ThongKeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QLKHO.Areas.ThongKes.Controllers
+{
+    [Area("ThongKes")]
+    [Route("/[controller]/[action]")]
+    public class ThongKeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
